8f1a9bdcad5fc70fca77c2a0f08dc0f2e1ca0864
relicright <relicright@hotmail.com>
2016-08-06 15:46:57 -0400

    big.float variables in the 'math' package

---
+++ math.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/big"
+)
+
+func main() {
+	fmt.Println(mathofsametype(22.2, 33.4))
+
+	// BIG.FLOAT ALLOWS FOR MORE ACCURATE CALCULATIONS
+	var b1, b2, b3, bigSum big.Float
+
+	//BIG.FLOAT VARIABLES ARE SET THROUGH DOT SYNTEX - IN THIS CASE .SETFLOAT64()
+	// These variables are being set as float64 data types
+	b1.SetFloat64(23.5)
+	b2.SetFloat64(33.5)
+	b3.SetFloat64(28.2)
+
+	//BIG FLOATS MATH OPERATIONS ARE THROUGH THE DOT SYNTEX - IN THIS CASE .ADD()
+	bigSum.Add(&b1, &b2).Add(&bigSum, &b3)
+	fmt.Printf("Bigsum equlas: %10g\n", &bigSum)
+
+	circleRadius := 15.5
+	circumference := circleRadius * math.Pi
+
+	fmt.Printf("Circumference Equals: %.2f\n", circumference)
+}
+
+// VARIABLES MUST BE OF THE SAME TYPE TO DO MATH OPERATIONS ON THEM
+func mathofsametype(x, y float64) float64 {
+	return x + y
+}
