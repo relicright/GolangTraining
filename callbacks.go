20e92de5c5f25a23f280e794395b2d0a5f1df9d1
relicright <relicright@hotmail.com>
2016-08-09 16:04:20 -0400

    callbacks in Go

---
+++ callbacks.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// A callback
+func visit(numbers []int, callback func(int))  {
+	for _, n := range numbers {
+		callback(n)
+	}
+}
+
+func main() {
+	visit([]int{1, 2, 3, 4}, func(n int){
+		fmt.Println(n)
+	})
+}
+
+
+//callback: passing a fun as an argument
