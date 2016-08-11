d78ba134ea64d8925b795ebf9d0382a13509ab8d
relicright <relicright@hotmail.com>
2016-08-11 19:29:59 -0400

    callbacks in go

--- callbacks.go
+++ callbacks.go
@@ -2,17 +2,22 @@ package main
 
 import "fmt"
 
-// A callback
+// A callback calls back to the function in the params
 func visit(numbers []int, callback func(int))  {
+
+	// Loop through the slices of 'numbers'
 	for _, n := range numbers {
+
+		// use the callback function in the arguments
 		callback(n)
 	}
 }
 
 func main() {
-	visit([]int{1, 2, 3, 4}, func(n int){
-		fmt.Println(n)
-	})
+
+	// Run the visit function and pass in a func(int) (a function that returns int)
+	// The function in this case uses print line using the variable 'n'
+	visit([]int{1, 2, 3, 4}, func(n int){fmt.Println(n)})
 }
 
 
