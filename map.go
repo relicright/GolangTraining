6e7e8c60e7ba52835423066277e85f2885fcb1ca
relicright <relicright@hotmail.com>
2016-08-12 21:55:18 -0400

    Maps in go

--- map.go
+++ map.go
@@ -1,1 +1,57 @@
-package GolangTraining
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+
+	fmt.Println(mapmaker("key", 1))
+
+	m2 := mapmaker("key2", 23)
+	fmt.Println(m2["key2"])
+
+	states := make(map[string]string)
+	fmt.Println(states)
+
+	states["WA"] = "Washington"
+	states["TX"] = "Texas"
+	states["CA"] = "California"
+
+	fmt.Println(states)
+
+	california := states["CA"]
+	fmt.Println(california)
+
+	delete(states, "CA")
+	fmt.Println(states)
+
+	states["NY"] = "New York"
+
+	for k, v := range states {
+		fmt.Printf("%v: %v\n", k, v)
+	}
+
+	keys := make([]string, len(states))
+
+	i := 0
+	for k:= range states {
+		keys[i] = k
+		i++
+	}
+
+	sort.Strings(keys)
+	fmt.Println("\nSorted")
+
+	for i := range keys {
+		fmt.Println(states[keys[i]])
+	}
+}
+
+func mapmaker(n string, i int) map[string]int {
+
+	m := make(map[string]int)
+	m[n] = i
+	return m
+}
--- map2.go
+++ map2.go
@@ -1,1 +1,45 @@
-package GolangTraining
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+
+	m := mapmaker2(22)
+
+	fmt.Println(m)
+
+	ms := make([]string, len(m))
+
+	i := 0
+	for names := range m {
+		ms[i] = names
+		i++
+	}
+
+	sort.Strings(ms)
+
+	for sortname := range ms {
+		fmt.Println(m[ms[sortname]])
+		fmt.Printf("%T %v", m[ms[sortname]], m[ms[sortname]])
+	}
+
+	fmt.Println(ms)
+}
+
+func mapmaker2(i int) map[string]int {
+
+	m := make(map[string]int)
+
+	m["key1"] = i + 1
+	m["key4"] = i + 4
+	m["key2"] = i + 2
+	m["key5"] = i + 5
+	m["key3"] = i + 3
+
+	fmt.Println(m)
+
+	return m
+}
