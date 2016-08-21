91693b705d2dd83d88deed97548b0bf4584798f0
relicright <relicright@hotmail.com>
2016-08-21 14:30:20 -0400

    JSON encoding in go

--- jsonencoding.go
+++ jsonencoding.go
@@ -1,1 +1,27 @@
-package GolangTraining
+package main
+
+import (
+	//"fmt"
+	"encoding/json"
+	"os"
+)
+
+type Person struct {
+
+	First string
+	Last string
+	Age int
+	notExported int
+}
+
+func main()  {
+
+	p1 := Person{"James", "Bond", 20, 007}
+
+	// NewEncoder returns a new *encoder that writes to w.
+	//
+	//Now that you have an *encoder you can use its receiver function 'Encode()'
+	// Encode writes the JSON encoding of v to the stream,
+	// followed by a newline character.
+	json.NewEncoder(os.Stdout).Encode(p1)
+}
