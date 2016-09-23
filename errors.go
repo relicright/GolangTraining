d18d2088aef05605490d2110d11e2676b5e15111
relicright <relicright@hotmail.com>
2016-09-23 16:51:24 -0400

    n

--- errors.go
+++ errors.go
@@ -1,1 +1,29 @@
-package GolangTraining
+package main
+
+import (
+	//"fmt"
+	"errors"
+	"log"
+)
+
+func main() {
+
+	//Catch the error from function 'sqrt()'
+	_, err := sqrt(-10)
+
+	//Display the error if one is returned
+	if err != nil{
+		log.Fatalln(err)
+	}
+}
+
+// sqrt will hold our intentional error to display
+func sqrt(f float64) (float64, error) {
+	if f < 0 {
+		//errors.New will be out created error with a custom string
+		return 0, errors.New("Some error here saying about how no square of less than 0")
+	}
+
+	// If the number is not below zero, then we return a random number along with nil
+	return 42, nil
+}
--- fileio.go
+++ fileio.go
@@ -1,1 +1,46 @@
-package GolangTraining
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"io"
+	"io/ioutil"
+)
+
+func main() {
+
+	content := "Hello from go!"
+
+	// To create a file with Go:
+	// identifier, error, then os.Create(path to your file)
+	file, err := os.Create("./fromString.txt")
+	// Check for any errors while creating the file
+	CheckError(err)
+
+	// Defer a 'file.Close()' statement so the stream is closed
+	// at the end
+	defer file.Close()
+
+	//Write to a file
+	// identifier, error.  Then, name of the file, content to enter
+	ln, err := io.WriteString(file, content)
+	// check for an error writing to the file
+	CheckError(err)
+
+	// convert 'content' to a slice of bytes '[]byte'
+	bytes := []byte(content)
+
+	// you can write to a file using an array of bytes by using the
+	// ioutils package.  WriteFile(Filename, data, permissions)
+	ioutil.WriteFile("./fromBytes.txt", bytes, 0644)
+
+	fmt.Println("All done with file of %v characters", ln)
+
+}
+
+func CheckError(e error)  {
+	if e != nil{
+		log.Fatalln(e)
+	}
+}
